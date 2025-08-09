01111f9283d555f5df11aa64ca04f2d42aa72c11
Timex <timexdev123@gmail.com>
2025-08-09 11:03:12 +0300

    500 Fix

--- api/hug.py
+++ api/hug.py
@@ -1,30 +1,23 @@
 from http.server import BaseHTTPRequestHandler
 import json
 
-# Глобальная переменная (сбросится при перезапуске сервера)
-hug_count = 0
-
 class Handler(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
         self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
-
+    
     def do_GET(self):
-        global hug_count
         self._set_headers()
-        self.wfile.write(json.dumps({"count": hug_count}).encode())
+        self.wfile.write(json.dumps({"count": 0}).encode())
 
     def do_POST(self):
-        global hug_count
-        hug_count += 1
         self._set_headers()
-        self.wfile.write(json.dumps({"count": hug_count}).encode())
+        self.wfile.write(json.dumps({"count": 1}).encode())
 
-# Для Vercel Serverless
 def handler(request, response):
-    h = Handler(request, {}, None)
+    h = Handler(request, response, None)
     if request.method == 'GET':
         h.do_GET()
     elif request.method == 'POST':
