7b7b3d560864ced4659843ce95945a17155f8f60
Timex <timexdev123@gmail.com>
2025-08-09 12:34:58 +0300

    Update hug.py

--- api/hug.py
+++ api/hug.py
@@ -1,27 +1,32 @@
+from http.server import BaseHTTPRequestHandler
 import json
+import os
 
-# Глобальная переменная для счетчика
-counter = 0
+counter = int(os.environ.get('HUG_COUNTER', '0'))
 
 def handler(request, response):
     global counter
     
-    # Устанавливаем заголовки
-    response.headers["Content-Type"] = "application/json"
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    
     # Обработка GET-запроса
     if request.method == "GET":
+        response.status_code = 200
+        response.headers["Content-Type"] = "application/json"
+        response.headers["Access-Control-Allow-Origin"] = "*"
         response.body = json.dumps({"count": counter}).encode()
         return response
-    
+
     # Обработка POST-запроса
     if request.method == "POST":
         counter += 1
+        os.environ['HUG_COUNTER'] = str(counter)
+        
+        response.status_code = 200
+        response.headers["Content-Type"] = "application/json"
+        response.headers["Access-Control-Allow-Origin"] = "*"
         response.body = json.dumps({"count": counter}).encode()
         return response
-    
+
     # Для других методов
-    response.status = 405
-    response.body = json.dumps({"error": "Method not allowed"}).encode()
+    response.status_code = 405
+    response.body = b'Method Not Allowed'
     return response
