fe5accf015e422a7eeed6e04848c54927e3864c4
Timex <timexdev123@gmail.com>
2025-08-09 11:20:58 +0300

    Style fix and 500 fix (please)

--- api/hug.py
+++ api/hug.py
@@ -1,24 +1,26 @@
 from http.server import BaseHTTPRequestHandler
 import json
 
+# Глобальная переменная для хранения состояния
+COUNTER = 0
+
 def handler(request, response):
     class HugHandler(BaseHTTPRequestHandler):
-        def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-
         def _set_headers(self):
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.send_header('Access-Control-Allow-Origin', '*')
             self.end_headers()
-
+        
         def do_GET(self):
             self._set_headers()
-            self.wfile.write(json.dumps({'count': 0}).encode())
-
+            self.wfile.write(json.dumps({'count': COUNTER}).encode())
+        
         def do_POST(self):
+            global COUNTER
+            COUNTER += 1
             self._set_headers()
-            self.wfile.write(json.dumps({'count': 1}).encode())
+            self.wfile.write(json.dumps({'count': COUNTER}).encode())
 
     h = HugHandler(request, response, None)
     if request.method == 'GET':
