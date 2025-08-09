a9e9b5bfd312d44fdc67b851c332220638f34763
Timex <timexdev123@gmail.com>
2025-08-09 11:17:58 +0300

    I FUCK THIS 500 ERROR

--- api/hug.py
+++ api/hug.py
@@ -1,25 +1,29 @@
 from http.server import BaseHTTPRequestHandler
 import json
 
-class Handler(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'application/json')
-        self.send_header('Access-Control-Allow-Origin', '*')
-        self.end_headers()
-    
-    def do_GET(self):
-        self._set_headers()
-        self.wfile.write(json.dumps({'count': 0}).encode())
-    
-    def do_POST(self):
-        self._set_headers()
-        self.wfile.write(json.dumps({'count': 1}).encode())
-
 def handler(request, response):
-    h = Handler(request, response, None)
+    class HugHandler(BaseHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+
+        def _set_headers(self):
+            self.send_response(200)
+            self.send_header('Content-type', 'application/json')
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+
+        def do_GET(self):
+            self._set_headers()
+            self.wfile.write(json.dumps({'count': 0}).encode())
+
+        def do_POST(self):
+            self._set_headers()
+            self.wfile.write(json.dumps({'count': 1}).encode())
+
+    h = HugHandler(request, response, None)
     if request.method == 'GET':
         h.do_GET()
     elif request.method == 'POST':
         h.do_POST()
+    
     return response
